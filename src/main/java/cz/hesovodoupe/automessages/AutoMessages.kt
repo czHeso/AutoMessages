a8f93fa9f11f097028094e9136a4a82a4ef0fc6f
Heso <admin@snezik.cz>
2024-02-23 12:52:55 +0100

    ReloadCMD fix - final D:

--- src/main/java/cz/hesovodoupe/automessages/AutoMessages.kt
+++ src/main/java/cz/hesovodoupe/automessages/AutoMessages.kt
@@ -2,17 +2,20 @@ package cz.hesovodoupe.automessages
 
 import ConfigManager
 import cz.foresttech.api.ColorAPI
-import cz.hesovodoupe.automessages.commands.reloadCmd
 import org.bstats.bukkit.Metrics
 import org.bukkit.Bukkit
 import org.bukkit.Sound
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.scheduler.BukkitRunnable
 
+
 private const val PLUGIN_ID = 20493
 private const val DEFAULT_SOUND_VOLUME = 1.0f
 private const val DEFAULT_SOUND_PITCH = 1.0f
+public var bukkitidTask: Int = 0
 
 class AutoMessages : JavaPlugin() {
 
@@ -31,45 +34,61 @@ class AutoMessages : JavaPlugin() {
         loadConfiguration()
 
         startMessageSendingTask()
-        registerCommands()
+        getCommand("automessage")?.setExecutor(this);
     }
 
-    private fun loadConfiguration() {
+    public fun loadConfiguration() {
         config = AutoMessagesConfig(configManager)
     }
 
     fun startMessageSendingTask() {
-        object : BukkitRunnable() {
+        bukkitidTask = object : BukkitRunnable() {
             override fun run() {
-                config.messageIndex = config.messageIndex % if (config.allowRaw) config.rawMessages.size else config.messages.size
-                val messageToSend = if (config.randomizeMessages) {
-                    if (config.allowRaw) config.rawMessages.random() else config.messages.random()
-                } else {
-                    if (config.allowRaw) config.rawMessages[config.messageIndex] else config.messages[config.messageIndex]
-                }
 
-                Bukkit.getOnlinePlayers().forEach {
-                    playSoundForPlayer(it, config.soundEffect)
-                    if (config.allowRaw) {
-                        val commandRaw = "tellraw @a $messageToSend"
-                        Bukkit.dispatchCommand(Bukkit.getConsoleSender(), commandRaw)
+                    config.messageIndex = config.messageIndex % if (config.allowRaw) config.rawMessages.size else config.messages.size
+                    val messageToSend = if (config.randomizeMessages) {
+                        if (config.allowRaw) config.rawMessages.random() else config.messages.random()
                     } else {
-                        it.sendMessage(ColorAPI.colorize(messageToSend))
+                        if (config.allowRaw) config.rawMessages[config.messageIndex] else config.messages[config.messageIndex]
                     }
+
+                    Bukkit.getOnlinePlayers().forEach {
+                        playSoundForPlayer(it, config.soundEffect)
+                        if (config.allowRaw) {
+                            val commandRaw = "tellraw @a $messageToSend"
+                            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), commandRaw)
+                        } else {
+                            it.sendMessage(ColorAPI.colorize(messageToSend))
+                        }
+                    }
+
+                    config.messageIndex += 1  // Increment after accessing the message
+
                 }
 
-                config.messageIndex += 1  // Increment after accessing the message
-            }
-        }.runTaskTimer(this, 0L, config.timeDelay * 20L)
+
+        }.runTaskTimer(this, 0L, config.timeDelay * 20L).taskId
     }
 
-
-    private fun registerCommands() {
-        getCommand("automessage")?.setExecutor(reloadCmd(this))
-    }
 
     fun playSoundForPlayer(player: Player, soundEffect: String) {
         player.playSound(player.location, Sound.valueOf(soundEffect), DEFAULT_SOUND_VOLUME, DEFAULT_SOUND_PITCH)
+    }
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
+        if (!sender.hasPermission("am.reload")) {
+            sender.sendMessage("You don't have permissions to use this command")
+            return true
+        }
+
+        reloadConfig()
+        saveConfig()
+        val id = bukkitidTask.toInt()
+        server.scheduler.cancelTask(id)
+        loadConfiguration()
+        sender.sendMessage("Config reloaded.")
+        startMessageSendingTask()
+        return true
     }
 }
 
@@ -95,3 +114,5 @@ data class AutoMessagesConfig(
         barMessages = configManager.barMessages()
     )
 }
+
+
